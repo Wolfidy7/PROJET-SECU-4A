721e167ac45666868d77a7fb95a70e0e7fcf4c89
smakalou <makaloucherif@gmail.com>
2024-04-21 15:18:43 +0200

    Work in progress

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ app.use('/api/users', userRoutes(pool));
 //   }
 // });
 
-app.listen(5000, () => {
-console.log('Server is running on port 5000');
+app.listen(3000, () => {
+console.log('Server is running on port 3000');
 });
 
