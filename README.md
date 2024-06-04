2614639f8c5b787e6a40c36b5a6543454fd1b5ed
smakalou <makaloucherif@gmail.com>
2024-06-04 19:07:39 +0200

    Access denied issue during authentication

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-### PROJET-SECU-4Asudo systemctl start curity
+### PROJET-SECU-4A
 
 Install PostgreSQL
 Create a database 
+
+### Run keycloak if you have created the docker volume keycloak_data
+
+sudo docker run -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v keycloak_data:/opt/keycloak/data quay.io/keycloak/keycloak:24.0.4 start-dev
+
+### Configure keycloak
+
+https://www.keycloak.org/getting-started/getting-started-docker
 
 ### Then execute the following commands, at the root of the project:sudo systemctl start curity
 
--- backend/server.js
+++ backend/server.js
@@ -19,39 +19,30 @@ app.use(express.json());
 app.use('/api/users', userRoutes(pool));
 ////////////////////////////////////////////////////CONF OPENID CONNECT////////////////////////////////////////////////
 
-// const express = require('express');
-// const session = require('express-session');
-// const Keycloak = require('keycloak-connect');
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
 
-// const app = express();
-// const memoryStore = new session.MemoryStore();
+const memoryStore = new session.MemoryStore();
 
-// app.use(session({
-//   secret: 'secret',
-//   resave: false,
-//   saveUninitialized: true,
-//   store: memoryStore
-// }));
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore
+}));
 
-// const keycloak = new Keycloak({ store: memoryStore }, 'keycloak.json');
+const keycloak = new Keycloak({ store: memoryStore }, 'keycloak.json');
 
-// app.use(keycloak.middleware());
+app.use(keycloak.middleware());
 
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the home page!');
-// });
+app.get('/login', keycloak.protect(), (req, res) => {
+  res.send('Hello, you are authenticated!');
+});
 
-// app.get('/login', keycloak.protect(), (req, res) => {
-//   res.send('Hello, you are authenticated!');
-// });
+app.get('/logout', (req, res) => {
+  keycloak.logout(req, res);
+});
 
-// app.get('/logout', (req, res) => {
-//   keycloak.logout(req, res);
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server is running on http://localhost:3000');
-// });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
